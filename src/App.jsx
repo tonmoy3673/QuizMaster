5412d76a314e20bc597c1ec302dc4618258031eb
MD. Mehedi Hasan Tonmoy <107616839+tonmoy3673@users.noreply.github.com>
2025-03-17 15:18:59 +0600

    :books: set bg color and google fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 
 import './App.css'
 import React from 'react';
+import Home from './Components/Home/Home';
 function App() {
 
-
   return (
-    <>
-  <h2>QuizMaster</h2>
-    </>
+    <div className='bg-gradient-to-r from-[#08001F] to-[#30197d] w-[100%] h-[100vh]'>
+    <Home/>
+    </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className=''>
+            {/* =========== Title ======= */}
+           <header>
+           <h1>Quiz Master App</h1>
+           </header>
+           {/* ============== section ======== */}
+           <section>
+            {/* ========== row 1 ======= */}
+        <div className="row-1">
+
+        </div>
+        {/* ========= row 2 ===== */}
+        <div className="row-2">
+
+        </div>
+           </section>
+        </div>
+    );
+};
+
+export default Home;
